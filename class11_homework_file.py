6433c44a0d1c91047ec18d9d5d8ab73abffe5f87
Rasel Khan <russel35-1170@diu.edu.bd>
2017-04-09 16:20:59 +0600

    Add files via upload

---
+++ class11_homework_file.py
@@ -0,0 +1,71 @@
+r =open ("E:\\pythonist-dp01-master\\class 4.py","r")
+classa = r.read()
+r.close()
+
+s = open("class 4.txt", "w")
+s.write(classa)
+s.close()
+
+
+r =open ("E:\\pythonist-dp01-master\\class_8.py","r")
+classa = r.read()
+r.close()
+
+s = open("class 8.txt", "w")
+s.write(classa)
+s.close()
+
+r =open ("E:\\pythonist-dp01-master\\class5.py","r")
+classa = r.read()
+r.close()
+
+s = open("class 5.txt", "w")
+s.write(classa)
+s.close()
+
+r =open ("E:\\pythonist-dp01-master\\class5_dictonary.py","r")
+classa = r.read()
+r.close()
+
+s = open("class 5 dictionary.txt", "w")
+s.write(classa)
+s.close()
+
+r =open ("E:\\pythonist-dp01-master\\class5_set.py","r")
+classa = r.read()
+r.close()
+
+s = open("class 5 set.txt", "w")
+s.write(classa)
+s.close()
+
+
+r =open ("E:\\pythonist-dp01-master\\class5_touple.py","r")
+classa = r.read()
+r.close()
+
+s = open("class 5 touple.txt", "w")
+s.write(classa)
+s.close()
+
+
+r =open ("E:\\pythonist-dp01-master\\class6.py","r")
+classa = r.read()
+r.close()
+
+s = open("class 6.txt", "w")
+s.write(classa)
+s.close()
+
+
+r =open ("E:\\pythonist-dp01-master\\class6_home work.py","r")
+classa = r.read()
+r.close()
+
+s = open("class 6 home work.txt", "w")
+s.write(classa)
+s.close()
+
+
+
+
---
+++ class11_homework_file2.py
@@ -0,0 +1,42 @@
+r = open("E:\\pythonist-dp01-master\\class6_loop.py", "r")
+classa = r.read()
+r.close()
+
+w = open("class 6 loop.txt", "w")
+w.write(classa)
+w.close()
+
+
+r = open("E:\\pythonist-dp01-master\\class7.py", "r")
+classa = r.read()
+r.close()
+
+w = open("class 7.txt", "w")
+w.write(classa)
+w.close()
+
+
+r = open("E:\\pythonist-dp01-master\\class7_homework.py", "r")
+classa = r.read()
+r.close()
+
+w = open("class 7 home work.txt", "w")
+w.write(classa)
+w.close()
+
+
+r = open("E:\\pythonist-dp01-master\\class8_homework.py", "r")
+classa = r.read()
+r.close()
+
+w = open("class 8 home work.txt", "w")
+w.write(classa)
+w.close()
+
+r = open("E:\\pythonist-dp01-master\\class_11.py", "r")
+classa = r.read()
+r.close()
+
+w = open("class 11.txt", "w")
+w.write(classa)
+w.close()
